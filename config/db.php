9bc5bf8bec99750f74e5c06b70b1ff23ce4bcf5b
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-03-21 19:04:08 +0000

    criacao do template yii2 e configuracao da db

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=gestorpedreira',
+    'username' => 'programador',
+    'password' => 'CTeSP-DS-Grupo03',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
