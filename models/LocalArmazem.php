676738b27ccac4e27228e0c0c23e79ae9cf76710
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-03-22 14:48:31 +0000

    crud local armazem

---
+++ models/LocalArmazem.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "localarmazem".
+ *
+ * @property int $id
+ * @property string $nome
+ *
+ * @property Lote[] $lotes
+ */
+class LocalArmazem extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'localarmazem';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome'], 'required'],
+            [['nome'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+        ];
+    }
+
+    /**
+     * Gets query for [[Lotes]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getLotes()
+    {
+        return $this->hasMany(Lote::className(), ['idLocalArmazem' => 'id']);
+    }
+}
---
+++ views/local-armazem/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\LocalArmazemSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Local Armazems';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="local-armazem-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Local Armazem', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'nome',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, LocalArmazem $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
