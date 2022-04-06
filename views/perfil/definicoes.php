100f8cc01f1643957e9588621af94ebfea7258ca
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-04-06 22:36:05 +0100

    controllers actions e views do perfil

---
+++ views/perfil/definicoes.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+
+/* @var $this yii\web\View */
+
+$this->title = 'Definições';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="definicoes-index">
+    <h1>Definições da Conta</h1>
+</div>
---
+++ views/perfil/historico.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+
+/* @var $this yii\web\View */
+
+$this->title = 'Histórico';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="historico-index">
+    <h1>Histórico de Encomendas</h1>
+</div>
