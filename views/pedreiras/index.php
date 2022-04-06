a1f56adfbc7c138a2ee94b36de0f971f339ae0a0
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-04-06 22:09:51 +0100

    controllerse views pedreiras

---
+++ views/pedreiras/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+
+/* @var $this yii\web\View */
+
+$this->title = 'Equipa';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="equipa-index">
+    <h1>Pedreira 1</h1>
+    <h1>Pedreira 2</h1>
+    <h1>Pedreira 3</h1>
+</div>
