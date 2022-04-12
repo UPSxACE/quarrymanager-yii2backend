a40d608ba2f375e7f795cdfb8f620f350362948c
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-04-12 21:12:30 +0100

    assets update

--- assets/3. correcaoBaseDeDados.sql
+++ assets/3. correcaoBaseDeDados.sql
@@ -7,3 +7,14 @@ AFTER full_name;
 ALTER TABLE profile
 ADD COLUMN genero tinyint
 AFTER full_name; 
+
+ALTER TABLE `gestorpedreira`.`produto` 
+ADD COLUMN `idFotografia` INT NULL AFTER `idCor`,
+ADD INDEX `fk_Produto_Fotografia_idx` (`idFotografia` ASC);
+;
+ALTER TABLE `gestorpedreira`.`produto` 
+ADD CONSTRAINT `fk_Produto_Fotografia1`
+  FOREIGN KEY (`idFotografia`)
+  REFERENCES `gestorpedreira`.`fotografia` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
