9916e6195b8d489836db5fd79c943c4008c2ff6e
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-04-13 18:11:17 +0100

    centralizei imagens da view da loja

--- views/loja/_produto.php
+++ views/loja/_produto.php
@@ -3,8 +3,8 @@ use app\models\Produto;
 
 /* @var Produto $model                     ------>                variável $model é automatica do yii2; convém adicionar*/
 ?>
-<div class="m-5" style="background-color:grey">
-    <img width="240px" height="160px" src="/uploads/<?=$model->idFotografia0->link?>">
+<div class="m-5 d-flex flex-column" style="background-color:grey">
+    <img class="align-self-center" width="240px" height="160px" src="/uploads/<?=$model->idFotografia0->link?>">
     <div class="row">
         <?php $formatter = \Yii::$app->formatter; ?>
         <span class="col-6"><?=$model->tituloArtigo?></span>
