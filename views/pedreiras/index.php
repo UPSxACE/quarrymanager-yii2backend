37f92b3d762fc22081593e43650e80a91445ccca
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-04-14 01:27:24 +0100

    att tittle

--- views/pedreiras/index.php
+++ views/pedreiras/index.php
@@ -6,11 +6,11 @@ use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 
-$this->title = 'Equipa';
+$this->title = 'Pedreiras';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="equipa-index">
+<div class="pedreiras-index">
     <h1>Pedreira 1</h1>
     <h1>Pedreira 2</h1>
     <h1>Pedreira 3</h1>
