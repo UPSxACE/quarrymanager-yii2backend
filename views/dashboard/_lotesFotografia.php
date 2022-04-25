b89dadf5a3a40d9beb4fbe7364a7a551009087f0
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-04-25 18:26:47 +0100

    imagem clicavel

--- views/dashboard/_lotesFotografia.php
+++ views/dashboard/_lotesFotografia.php
@@ -3,5 +3,5 @@ use app\models\FotografiaProduto;
 
 /* @var Produto $model                     ------>                variável $model é automatica do yii2; convém adicionar*/
 ?>
-<img class="align-self-center" width="100%" height="160px" style="object-fit: cover" src="/uploads/<?= $model->idFotografia0->link ?>">
+<a target="_blank" href="/uploads/<?= $model->idFotografia0->link ?>"><img class="align-self-center" width="100%" height="160px" style="object-fit: cover" src="/uploads/<?= $model->idFotografia0->link ?>"></a>
 
