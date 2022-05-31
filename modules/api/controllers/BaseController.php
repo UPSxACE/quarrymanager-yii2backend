c2cacfe65ca49d158aa54a39293f9f59c59a6e38
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-05-31 19:23:41 +0100

    cors configuraçao

--- modules/api/controllers/BaseController.php
+++ modules/api/controllers/BaseController.php
@@ -15,6 +15,47 @@ class BaseController extends ActiveController
     {
 
         $behaviors = parent::behaviors();
+
+
+        //$auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+            'cors' => [
+                // restrict access to
+                //'Origin' => ['http://localhost:3000', 'https://localhost:3000'],
+                'Origin' => ['*'],
+
+                // Allow only POST and PUT methods
+                ////'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'OPTIONS'],
+                // Allow only headers 'X-Wsse'
+                //'Access-Control-Request-Headers' => ['X-Wsse'],
+                'Access-Control-Request-Headers' => ['*'],
+
+
+                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                ////'Access-Control-Allow-Credentials' => true,
+                // Allow OPTIONS caching
+                ////'Access-Control-Max-Age' => 3600,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                ////'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+            ],
+        ];
+
+        //$behaviors['authenticator'] = $auth;
+        $behaviors['authenticator'] = [
+
+            'class' => CompositeAuth::class,
+            'authMethods' => [
+                HttpBasicAuth::class,
+                HttpBearerAuth::class //mecanismo de autenticação que o JWT vai utilizar
+            ]
+        ];
+
+
+        $behaviors['authenticator']['except'] = ['options'];
+
         $behaviors['access'] = [
             'class' => AccessControl::className(),
             'rules' => [
@@ -29,36 +70,6 @@ class BaseController extends ActiveController
             'denyCallback' => function ($rule, $action) {
                 throw new ForbiddenHttpException("You're not allowed to access");
             }];
-
-        $behaviors['authenticator'] = [
-
-            'class' => CompositeAuth::class,
-            'authMethods' => [
-                HttpBasicAuth::class,
-                HttpBearerAuth::class //mecanismo de autenticação que o JWT vai utilizar
-            ]
-        ];
-
-        $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class,
-            'cors' => [
-                // restrict access to
-                'Origin' => ['http://localhost:3000/', 'https://localhost:3000/'],
-                // Allow only POST and PUT methods
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT'],
-                // Allow only headers 'X-Wsse'
-                //'Access-Control-Request-Headers' => ['X-Wsse'],
-                'Access-Control-Request-Headers' => ['*'],
-
-
-                // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
-                'Access-Control-Allow-Credentials' => true,
-                // Allow OPTIONS caching
-                'Access-Control-Max-Age' => 3600,
-                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-                'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
-            ],
-        ];
 
         return $behaviors;
 
