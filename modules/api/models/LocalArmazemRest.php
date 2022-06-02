33d17cdc9f1e98823e411b86690a4cd537d2044a
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-06-02 16:40:28 +0100

    Serviço Rest página Locais Armazém

--- modules/api/models/LocalArmazemRest.php
+++ modules/api/models/LocalArmazemRest.php
@@ -3,8 +3,28 @@
 namespace app\modules\api\models;
 
 use app\models\LocalArmazem;
+use yii\data\ActiveDataProvider;
 
 class LocalArmazemRest extends LocalArmazem
 {
+    public function dadosListar($params){
+        $query = LocalArmazemRest::find();
+        //  ->innerJoinWith('idCor0');
+        //->joinWith(['idMaterial0']);
 
+
+        $dataProvider = new ActiveDataProvider([
+            'query'=>$query
+        ]);
+
+        $this->load($params, "");
+
+        return $dataProvider;
+    }
+
+
+    public function fields(){
+        return ['nome'];
+
+    }
 }
