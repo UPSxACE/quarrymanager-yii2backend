e2abadbbf577620efcbbaae679d1470b3ebec4c3
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-06-15 17:49:02 +0100

    EndPoint Lote/Add

--- modules/api/controllers/LoteController.php
+++ modules/api/controllers/LoteController.php
@@ -14,7 +14,7 @@ class LoteController extends BaseController
     public function behaviors(){
         $behaviors = parent::behaviors();
         $behaviors['access']['rules'][] = [
-            'actions' =>  ['index', 'view', 'create', 'update', 'delete', 'options', 'listar' ],
+            'actions' =>  ['index', 'view', 'create', 'update', 'delete', 'options', 'listar', 'add' ],
             'allow' => true,
             'roles' => ['operario'] // se tirar o role, qualquer utilizar AUTENTICADO pode usar o serviço.
         ];
@@ -28,5 +28,12 @@ class LoteController extends BaseController
         $dataProvider = $model->dadosListar($get);
 
         return $dataProvider;
+    }
+
+    public function actionAdd(){
+        $modelLote = new LoteRest();
+        $modelLote->load(Yii::$app->request->post(), '');
+        $modelLote->save();
+        return $modelLote;
     }
 }
