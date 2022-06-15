733a7046a75ac7f62d2035fe468783586e3e3bd9
Diogo Jose Branquinho Carvalho <a44765@alunos.ipb.pt>
2022-06-15 22:28:55 +0100

    endpoint/actionLocalArmazemEditar

--- modules/api/controllers/LocalArmazemController.php
+++ modules/api/controllers/LocalArmazemController.php
@@ -14,7 +14,7 @@ class LocalArmazemController extends BaseController
     public function behaviors(){
         $behaviors = parent::behaviors();
         $behaviors['access']['rules'][] = [
-            'actions' =>  ['index', 'view', 'create', 'update', 'delete', 'options', 'listar', 'add' ],
+            'actions' =>  ['index', 'view', 'create', 'update', 'delete', 'options', 'listar', 'add', 'add' ],
             'allow' => true,
             'roles' => ['operario'] // se tirar o role, qualquer utilizar AUTENTICADO pode usar o serviço.
         ];
@@ -33,6 +33,13 @@ class LocalArmazemController extends BaseController
     public function actionAdd(){
         $model = new LocalArmazemRest();
         $model->load(Yii::$app->request->post(), '');
+        $model->save();
+        return $model;
+    }
+
+    public function actionEditar(){
+        $model = LocalArmazemRest::find()->where(['id' =>Yii::$app->request->post('id')])->one();
+        $model->load(yii::$app->request->post(), '');
         $model->save();
         return $model;
     }
