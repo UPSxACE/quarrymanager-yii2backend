3da5ceddd07996f9aab8138b9ad73869ffec6cb2
Diogo Jose Branquinho Carvalho <a44765@alunos.ipb.pt>
2022-06-19 17:18:29 +0100

    action edit cor

--- modules/api/controllers/CorController.php
+++ modules/api/controllers/CorController.php
@@ -66,9 +66,17 @@ class CorController extends BaseController
     }
 
     public function actionEditar(){
+
+        $access_header = Yii::$app->request->headers->get("Authorization");
+        $access_token = str_replace("Basic ", "", $access_header);
+        $access_token = base64_decode($access_token);
+        $access_token = str_replace(":", "", $access_token);
+        $user = UserRest::findOne(["access_token"=>$access_token]);
+
         $model = CorRest::find()->where(['id' =>Yii::$app->request->post('id')])->one();
         $model->load(yii::$app->request->post(), '');
         $model->save();
+        Logs::registrarLogUser($user->id, 2, "A cor" . $model->codigo_lote . " foi modificada.");
         return $model;
     }
 
