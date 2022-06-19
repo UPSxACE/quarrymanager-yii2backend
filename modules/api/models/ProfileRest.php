6843cb15bdd2bc5677a4e16bd0dd5eda884180e7
Diogo Jose Branquinho Carvalho <a44765@alunos.ipb.pt>
2022-06-19 16:26:07 +0100

    extrafieds profile

--- modules/api/models/ProfileRest.php
+++ modules/api/models/ProfileRest.php
@@ -7,6 +7,11 @@ use app\models\Profile;
 class ProfileRest extends Profile
 {
     public function fields(){
-        return ['full_name', 'created_at', 'genero', 'morada', 'dataNascimento', 'localidade', 'codPostal', 'telefone', 'email' ];
+        return ['full_name', 'created_at', 'genero', 'morada', 'dataNascimento', 'localidade', 'codPostal', 'telefone', 'email'];
+    }
+
+    public function extraFields()
+    {
+        return ['username'=> function ($model) {return $model->user->username;}];
     }
 }
