d12cd5c725518673b47f13aca776401f9758c583
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-10-22 16:47:43 +0100

    setting up testing environment

--- config/test.php
+++ config/test.php
@@ -1,42 +1,200 @@
 <?php
+
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/test_db.php';
+$email_params = (require __DIR__ . "/params.php")['email'];
 
-/**
- * Application configuration shared by all test types
- */
-return [
-    'id' => 'basic-tests',
+$config = [
+    'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'language' => 'en-US',
+
+    'timeZone' => 'Europe/London',
+
     'components' => [
-        'db' => $db,
-        'mailer' => [
-            'useFileTransport' => true,
-        ],
-        'assetManager' => [
-            'basePath' => __DIR__ . '/../web/assets',
-        ],
-        'urlManager' => [
-            'showScriptName' => true,
-        ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-        ],
+
         'request' => [
-            'cookieValidationKey' => 'test',
-            'enableCsrfValidation' => false,
-            // but if you absolutely need it set cookie domain to localhost
-            /*
-            'csrfCookie' => [
-                'domain' => 'localhost',
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'lVZtY6X1loBW9EuIRX6PAcqKCQtUpXJI',
+            //código para o aceitar dados em json:
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        /*
+        'user' => [
+            'identityClass' => 'app\models\Utilizador',
+            'enableAutoLogin' => true,
+        ],
+        */
+        'user' => [
+            'class' => 'amnah\yii2\user\components\User',
+            //'identityClass' => 'app\models\User',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        /*
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'encryption' => 'ssl',
+                'host' => 'smtp.sapo.pt',
+                'port' => '587',
+                'username' => 'gestorapedreira@sapo.pt',
+                'password' => 'CTeSP-DS-Grupo03'//'UTCEZ7QYjD9LHwJ4'
             ],
-            */
+        ],
+        */
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'encryption' => $email_params['transport']['encryption'],
+                'host' => $email_params['transport']['host'],
+                'port' => $email_params['transport']['port'],
+                'username' => $email_params['transport']['username'],
+                'password' => $email_params['transport']['password'],
+            ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+
+        'urlManager' => [
+
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+
+
+                'loja/produto/<id:\d+>' => 'loja/produto',
+                'dashboard/encomendas/<id:\d+>' => 'dashboard/encomendas-action',
+                'dashboard/encomendas/<id:\d+>/mobilizacao' => 'dashboard/encomendas-mobilizacao',
+                'dashboard/encomendas/<idEncomenda:\d+>/cancelar' => 'dashboard/cancelar-encomenda',
+                'dashboard/encomendas/<idEncomenda:\d+>/agendar-recolha' => 'dashboard/encomendas-agendar',
+                'dashboard/encomendas/<id:\d+>/step' => 'dashboard/encomendas-next-step',
+                'dashboard/encomendas/<idEncomenda:\d+>/mobilizacao/confirmar-recolha/<idRecolha:\d+>' => 'dashboard/confirmar-recolha',
+                'dashboard/lotes/<codigo_lote:\w+>' => 'dashboard/lotes-action',
+
+            ],
+        ],
+        'formatter' => [
+            'decimalSeparator' => ',',
+            'thousandSeparator' => ' ',
+            'currencyCode' => '€',
         ],
     ],
+    /* added because of amnah/yii2-user */
+    'modules' => [
+        'user' => [
+            'class' => 'amnah\yii2\user\Module',
+            // set custom module properties here ...
+            'requireEmail' => false,
+            'requireUsername' => true,
+            'modelClasses' => [
+                'Role' => 'app\models\Role',
+                'User' => 'app\models\User',
+            ]
+        ],
+        'api' => [
+            'class' => 'app\modules\api\RestAPI',
+            // ... other configurations for the module ...
+            'components' => [
+                'urlManager' => [
+                    'class' => 'yii\web\UrlManager',
+                    'enablePrettyUrl' => true,
+                    'showScriptName' => false,
+                    'rules' => [
+                        [
+                            /*
+                            'loja/produto/<id:\d+>' => 'loja/produto',
+                            'dashboard/encomendas/<id:\d+>' => 'dashboard/encomendas-action',
+                            'dashboard/encomendas/<id:\d+>/mobilizacao' => 'dashboard/encomendas-mobilizacao',
+                            'dashboard/encomendas/<idEncomenda:\d+>/cancelar' => 'dashboard/cancelar-encomenda',
+                            'dashboard/encomendas/<idEncomenda:\d+>/agendar-recolha' => 'dashboard/encomendas-agendar',
+                            'dashboard/encomendas/<id:\d+>/step' => 'dashboard/encomendas-next-step',
+                            'dashboard/encomendas/<idEncomenda:\d+>/mobilizacao/confirmar-recolha/<idRecolha:\d+>' => 'dashboard/confirmar-recolha',
+                            'dashboard/lotes/<codigo_lote:\w+>' => 'dashboard/lotes-action',
+                            */
+
+                            //serviços
+                            'class' => 'yii\rest\UrlRule',
+                            'controller' => [
+                                'api/auth',
+                                'api/cor',
+                                'api/local-armazem',
+                                'api/local-extracao',
+                                'api/logs',
+                                'api/lote',
+                                'api/material',
+                                'api/produto',
+                                'api/transportadora',
+                                'api/user'
+                                //'OPTIONS api/<module:\w+>s/<action>' => 'api/base/options',
+                                //'api/material',
+                            ],
+                            'pluralize' => false,
+                        ]
+
+                    ],
+                ],
+                'request' => [
+                    'class'=> 'yii\web\Request',
+                    // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                    'cookieValidationKey' => 'lVZtY6X1loBW9EuIRX6PAcqKCQtUpXJI',
+                    //código para o aceitar dados em json:
+                    'parsers' => [
+                        'application/json' => 'yii\web\JsonParser',
+                        'multipart/form-data' => 'yii\web\MultipartFormDataParser',
+                    ]
+                ],
+            ]
+        ],
+    ],
+    /* ^ added because of amnah/yii-user ^ */
+
     'params' => $params,
 ];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
--- modules/api/models/EstadoPedidoRest.php
+++ modules/api/models/EstadoPedidoRest.php
@@ -67,8 +67,8 @@ class EstadoPedidoRest extends EstadoPedido
         $ultimo_6 = date('Y-m-d', strtotime("last day of -5 month"));
         $meses = [$ultimo_1, $ultimo_2, $ultimo_3, $ultimo_4,$ultimo_5, $ultimo_6];
 
-        foreach ($meses as &$mes){
-            $mes = [date('m', strtotime($mes)), date('Y', strtotime($mes))];
+        foreach ($meses as $index=>$mes){
+            $meses[$index] = [date('m', strtotime($mes)), date('Y', strtotime($mes))];
         }
 
         $mes1 = EstadoPedidoRest::find()
@@ -77,38 +77,41 @@ class EstadoPedidoRest extends EstadoPedido
             ->andWhere(["month(estado_pedido.dataEstado)" => $meses[0][0]])
             ->andWhere(["year(estado_pedido.dataEstado)" => $meses[0][1]])
             ->count();
+
         $mes2 = EstadoPedidoRest::find()
             ->andWhere(['last' => '1'])->andWhere(['>=', 'idEstado', 8])
             ->andWhere(['last' => '1'])->andWhere(['<', 'idEstado', 10])
             ->andWhere(["month(estado_pedido.dataEstado)" => $meses[1][0]])
             ->andWhere(["year(estado_pedido.dataEstado)" => $meses[1][1]])
             ->count();
+
         $mes3 = EstadoPedidoRest::find()
             ->andWhere(['last' => '1'])->andWhere(['>=', 'idEstado', 8])
             ->andWhere(['last' => '1'])->andWhere(['<', 'idEstado', 10])
             ->andWhere(["month(estado_pedido.dataEstado)" => $meses[2][0]])
             ->andWhere(["year(estado_pedido.dataEstado)" => $meses[2][1]])
             ->count();
+
         $mes4 = EstadoPedidoRest::find()
             ->andWhere(['last' => '1'])->andWhere(['>=', 'idEstado', 8])
             ->andWhere(['last' => '1'])->andWhere(['<', 'idEstado', 10])
             ->andWhere(["month(estado_pedido.dataEstado)" => $meses[3][0]])
             ->andWhere(["year(estado_pedido.dataEstado)" => $meses[3][1]])
             ->count();
+
         $mes5 = EstadoPedidoRest::find()
             ->andWhere(['last' => '1'])->andWhere(['>=', 'idEstado', 8])
             ->andWhere(['last' => '1'])->andWhere(['<', 'idEstado', 10])
             ->andWhere(["month(estado_pedido.dataEstado)" => $meses[4][0]])
             ->andWhere(["year(estado_pedido.dataEstado)" => $meses[4][1]])
             ->count();
+
         $mes6 = EstadoPedidoRest::find()
             ->andWhere(['last' => '1'])->andWhere(['>=', 'idEstado', 8])
             ->andWhere(['last' => '1'])->andWhere(['<', 'idEstado', 10])
             ->andWhere(["month(estado_pedido.dataEstado)" => $meses[5][0]])
             ->andWhere(["year(estado_pedido.dataEstado)" => $meses[5][1]])
             ->count();
-
-
 
         return [0 => $mes1, -1 => $mes2, -2 => $mes3, -3 => $mes4, -4 => $mes5, -5 => $mes6];
     }
