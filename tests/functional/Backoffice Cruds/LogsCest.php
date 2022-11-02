29c941a9807a961f4f05caff1aaa15b2ca955993
Gabriel Afonso Pires <a50400@alunos.ipb.pt>
2022-11-02 18:17:29 +0000

    Teste Logs(create/update)

--- tests/functional/Backoffice Cruds/LogsCest.php
+++ tests/functional/Backoffice Cruds/LogsCest.php
@@ -25,11 +25,11 @@ class LogsCest
 
     public function updateTest(\FunctionalTester $I)
     {
-        $I->amOnPage(['logs/update?id=67']);
-        $I->fillField(["name"=>'Logs[idUser]'],1);
+        $I->amOnPage(['logs/update?id=1']);
+        $I->fillField(["name"=>'Logs[idUser]'],5);
         $I->fillField(["name"=>'Logs[idTipoAcao]'],2);
-        $I->fillField(["name"=>'Logs[descricao]'],"O local de armazém 'Areeiro de Bragança' foi criado.123");
-        $I->fillField(["name"=>'Logs[dataHora]'],"2022-11-02 15:07:00");
+        $I->fillField(["name"=>'Logs[descricao]'],"O local de armazém 'Areeiro do Monte' foi criado.");
+        $I->fillField(["name"=>'Logs[dataHora]'],"2022-11-02 18:08:00");
         $I->click('Save', 'button');
     }
 }
