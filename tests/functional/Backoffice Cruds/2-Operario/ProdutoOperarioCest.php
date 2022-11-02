562c74c17afe2ceb9f1d2269071259a0263a2344
Gabriel Afonso Pires <a50400@alunos.ipb.pt>
2022-11-02 21:13:39 +0000

    Teste Produto(create/update)

--- tests/functional/Backoffice Cruds/2-Operario/ProdutoOperarioCest.php
+++ tests/functional/Backoffice Cruds/2-Operario/ProdutoOperarioCest.php
@@ -39,4 +39,19 @@ class ProdutoOperarioCest extends ProdutoClienteCest
         $I->fillField(["name"=>'Produto[preco]'],20.69);
         $I->click('Save', 'button');
     }
+
+    public function updateTest(\FunctionalTester $I)
+    {
+        $I->amOnPage(['produto/update?id=1']);
+        $I->fillField(["name"=>'Produto[idMaterial]'],1);
+        $I->fillField(["name"=>'Produto[idCor]'],1);
+        $I->fillField(["name"=>'Produto[Res_Compressao]'],5.10);
+        $I->fillField(["name"=>'Produto[Res_Flexao]'],6.70);
+        $I->fillField(["name"=>'Produto[Massa_Vol_Aparente]'],3.4);
+        $I->fillField(["name"=>'Produto[Absorcao_Agua]'],69.69);
+        $I->fillField(["name"=>'Produto[tituloArtigo]'],"Areia");
+        $I->fillField(["name"=>'Produto[descricaoProduto]'],"Areia Sedimentar");
+        $I->fillField(["name"=>'Produto[preco]'],20.69);
+        $I->click('Save', 'button');
+    }
 }
